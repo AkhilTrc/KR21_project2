22cd9f01759e0108ba3cedb3f3c611099880048f
AkhilTrc <https://github.com/AkhilTrc/KR21_project2>
2021-11-30 12:17:47 +0100

    commiting BNReasoner.py at 12:16

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,5 +1,7 @@
 from typing import Union
 from BayesNet import BayesNet
+import networkx as nx   #####################################
+import matplotlib.pyplot as plt #################################
 
 
 class BNReasoner:
@@ -16,3 +18,16 @@ class BNReasoner:
             self.bn = net
 
     # TODO: This is where your methods should go
+bn_graph = BNReasoner("testing/dog_problem.BIFXML")     # creates object bn_graph
+
+# print (bn_graph.bn.get_all_variables())
+# print (bn_graph.bn.get_children("family-out"))
+# print (bn_graph.bn.get_cpt("family-out"))
+# print (bn_graph.bn.get_all_cpts())
+# print (bn_graph.bn.get_interaction_graph())
+
+# nx.draw(bn_graph.bn.get_interaction_graph(), with_labels=True, node_size=3000)
+# plt.show()
+
+bn_graph.bn.draw_structure()
+
